ebe629ffd0253644b0da9d9d18f73a832ff0b353
Simran Sethi <sethsimran84@gmail.com>
2017-09-01 16:47:56 +0530

    first commit

---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ActiveRecord::Base
+  belongs_to :article
+  has_attached_file :photo
+  validates_attachment_content_type :photo,
+ :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+end
